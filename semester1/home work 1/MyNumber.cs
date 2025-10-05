1f793b9ebfc9e28bd5dd5e6d7c97ac5ac9d02422
Vladislav Boiko <130917759+DeveloperVladislavBoiko@users.noreply.github.com>
2025-10-05 19:06:04 +0300

    Add files via upload

---
+++ semester1/home work 1/MyNumber.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Numerics;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks.Dataflow;
+public class MyNumber {
+    private int _value;
+    public MyNumber()
+    {
+        _value = 0;
+    }
+    public MyNumber(int value)
+    {
+        if (CorectnessInputArabicNumber(value)) _value = value;
+    }
+    public MyNumber(string inputValue)
+    {
+        string value = CorectnessInputRomeNumber(inputValue);
+        _value = TranslateToArabic(value);
+    }
+    public string RomeNumberPerformance
+    {
+        get => TranslateToRome();
+        set => _value = TranslateToArabic(
+            CorectnessInputRomeNumber(value));
+    }
+    public int ArabicNumberPerfomance
+    {
+        get => _value;
+        set
+        {
+            if (CorectnessInputArabicNumber(value)) _value = value;
+        }
+    }
+    static private Dictionary<int, string> ArabicToRomeTable
+    = new Dictionary<int, string>
+    {
+        {1000,"M" },
+        {900,"CM" },
+        {500,"D" },
+        {400,"CD" },
+        {100,"C" },
+        {90,"XC" },
+        {50,"L" },
+        {40,"XL" },
+        {10,"X" },
+        {9,"IX" },
+        {5,"V" },
+        {4,"IV" },
+        {1,"I" }
+    };
+    static private List<int> ListKeysArabicToRomeTable
+    = ArabicToRomeTable.Keys.ToList<int>();
+    private string TranslateToRome()
+    {
+        string symbolOfNegativity = (_value<0) ?"-":"";   
+        string romeNumber = "";
+        int value = Math.Abs(_value);
+        int len = ArabicToRomeTable.Count();
+        for (int index = 0; index < len; index++)
+        {
+            while (value >= ListKeysArabicToRomeTable[index])
+            {
+                romeNumber += ArabicToRomeTable[
+                    ListKeysArabicToRomeTable[index]];
+                value -= ListKeysArabicToRomeTable[index];
+            }
+        }
+        if (romeNumber == "") return "0";
+        return symbolOfNegativity+romeNumber;
+    }
+    static private Dictionary<char, int> RomeToArabicTable
+    = new Dictionary<char, int>
+    {
+        {'M',1000 },
+        {'D',500 },
+        {'C',100 },
+        {'L',50 },
+        {'X', 10},
+        {'V', 5},
+        {'I', 1},
+        {'0', 0},
+    };
+    private static List<char> ListKeysRomeToArabicTable =
+    RomeToArabicTable.Keys.ToList();
+    private int TranslateToArabic(string romeNumber)
+    {
+        int symbolOfNegativity = 1;
+        string romeValue = romeNumber.Replace(" ","");
+        if (romeNumber[0] == '-')
+        {
+            symbolOfNegativity = -1;
+            romeValue = romeNumber.Substring(1).Replace(" ","");
+        }
+        int arabicValue = 0;
+        int prevValueChar = 0;
+        int strSize = romeValue.Length;
+        for (int index = 0; index < strSize; index++)
+        {
+            int thisValueChar = RomeToArabicTable[romeValue[strSize - 1 - index]];
+            if (thisValueChar < prevValueChar)
+            {
+                arabicValue -= thisValueChar;
+            }
+            else
+            {
+                arabicValue += thisValueChar;
+            }
+            prevValueChar = thisValueChar;
+        }
+        return arabicValue*symbolOfNegativity;
+    }
+    private int TranslateToArabic()
+    {
+        return _value;
+    }
+    public override string ToString()
+    {
+        return $"Arabic number: {TranslateToArabic()}, Rome number: {TranslateToRome()}";
+    }
+    private static string CorectnessInputRomeNumber(string inputValue)
+    {
+        string value = inputValue.Replace(" ", "");
+        for (int index = 0; index < value.Length; index++)
+        {
+            if (!(ListKeysRomeToArabicTable.Contains(value[index])) && (value[index] != '-'))
+                throw new ArgumentException("НЕ известный символ,",
+                " используете символы для обозначения римских чисел");
+        }
+        return value;
+    }
+    
+    private static bool CorectnessInputArabicNumber (int value)
+    {
+        if (value < -4000 || value > 4000)
+            throw new ArgumentException("Числа вне диапозона от -4000 до 4000");
+        else return true;
+    }
+    public static MyNumber operator +(MyNumber n1, MyNumber n2)
+    => new MyNumber(n1._value + n2._value);
+    public static MyNumber operator -(MyNumber n1, MyNumber n2)
+    => new MyNumber(n1._value - n2._value);
+    public static MyNumber operator *(MyNumber n1, MyNumber n2)
+    => new MyNumber(n1._value * n2._value);
+    public static MyNumber operator /(MyNumber n1, MyNumber n2)
+    => new MyNumber(n1._value / n2._value);
+    public static bool operator ==(MyNumber n1, MyNumber n2)
+    => n1.ArabicNumberPerfomance == n2.ArabicNumberPerfomance;
+    public static bool operator !=(MyNumber n1, MyNumber n2)
+    => n1.ArabicNumberPerfomance != n2.ArabicNumberPerfomance;
+    public static bool operator < (MyNumber n1, MyNumber n2)
+    => n1.ArabicNumberPerfomance < n2.ArabicNumberPerfomance;
+    public static bool operator >(MyNumber n1, MyNumber n2)
+    => n1.ArabicNumberPerfomance > n2.ArabicNumberPerfomance;
+    public static bool operator >=(MyNumber n1, MyNumber n2)
+    => n1.ArabicNumberPerfomance >= n2.ArabicNumberPerfomance;
+    public static bool operator <=(MyNumber n1, MyNumber n2)
+    => n1.ArabicNumberPerfomance <= n2.ArabicNumberPerfomance;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is MyNumber n)
+        {
+            return n.ArabicNumberPerfomance == ArabicNumberPerfomance;
+        }
+        // if (obj is double n)
+        // {
+        //     return n == ArabicNumberPerfomance;
+        // }//////////добавить после добавления explicit и implicit
+        return false;
+    }
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(ArabicNumberPerfomance, RomeNumberPerformance);
+    }
+}
---
+++ semester1/home work 1/Program.cs
@@ -0,0 +1,108 @@
+//"Код работает с числами в диапозоне от -4000 до 4000, работает с арабскими и римскими числами"
+//спросить про то, нужно ли в стучае не корректной работы со свойством вызывать ошибку
+//файл с демонстрацией работы класса Program.cs я сделал с помощью ИИ, сам класс я делал полностью сам
+
+using System;
+// 1. Демонстрация конструкторов
+Console.WriteLine("1. СОЗДАНИЕ ОБЪЕКТОВ:");
+Console.WriteLine("====================");
+
+MyNumber num1 = new MyNumber(); // конструктор по умолчанию
+Console.WriteLine($"Конструктор по умолчанию: {num1}");
+
+MyNumber num2 = new MyNumber(42); // конструктор с арабским числом
+Console.WriteLine($"Конструктор с числом 42: {num2}");
+
+MyNumber num3 = new MyNumber("XIV"); // конструктор с римским числом
+Console.WriteLine($"Конструктор с 'XIV': {num3}");
+
+MyNumber num4 = new MyNumber(-123); // отрицательное число
+Console.WriteLine($"Конструктор с -123: {num4}");
+
+MyNumber num5 = new MyNumber("MCMXC"); // 1990
+Console.WriteLine($"Конструктор с 'MCMXC': {num5}");
+
+Console.WriteLine();
+
+// 2. Демонстрация свойств
+Console.WriteLine("2. СВОЙСТВА:");
+Console.WriteLine("============");
+
+MyNumber num = new MyNumber(256);
+Console.WriteLine($"Исходное число: {num}");
+
+num.ArabicNumberPerfomance = 512; // изменение через арабское свойство
+Console.WriteLine($"После установки ArabicNumberPerfomance = 512: {num}");
+
+num.RomeNumberPerformance = "CCL"; // изменение через римское свойство
+Console.WriteLine($"После установки RomeNumberPerformance = 'CCL': {num}");
+
+Console.WriteLine();
+
+// 3. Демонстрация арифметических операций
+Console.WriteLine("3. АРИФМЕТИЧЕСКИЕ ОПЕРАЦИИ:");
+Console.WriteLine("===========================");
+
+MyNumber a = new MyNumber(10);
+MyNumber b = new MyNumber("III");
+
+Console.WriteLine($"a = {a}, b = {b}");
+Console.WriteLine($"a + b = {a + b}");
+Console.WriteLine($"a - b = {a - b}");
+Console.WriteLine($"a * b = {a * b}");
+Console.WriteLine($"a / b = {a / b}");
+
+// Более сложные вычисления
+MyNumber x = new MyNumber("X"); // 10
+MyNumber y = new MyNumber("V"); // 5
+MyNumber z = new MyNumber("II"); // 2
+
+MyNumber result = (x + y) * z;
+Console.WriteLine($"\nСложное выражение: (X + V) * II = {result}");
+
+Console.WriteLine();
+
+// 4. Демонстрация операций сравнения
+Console.WriteLine("4. ОПЕРАЦИИ СРАВНЕНИЯ:");
+Console.WriteLine("======================");
+
+MyNumber n1 = new MyNumber(15);
+MyNumber n2 = new MyNumber("XV");
+MyNumber n3 = new MyNumber(20);
+
+Console.WriteLine($"n1 = {n1}, n2 = {n2}, n3 = {n3}");
+Console.WriteLine($"n1 == n2: {n1 == n2}");
+Console.WriteLine($"n1 != n3: {n1 != n3}");
+Console.WriteLine($"n1 < n3: {n1 < n3}");
+Console.WriteLine($"n3 > n1: {n3 > n1}");
+Console.WriteLine($"n1 >= n2: {n1 >= n2}");
+Console.WriteLine($"n1 <= n3: {n1 <= n3}");
+
+Console.WriteLine();
+
+// 5. Демонстрация метода Equals
+Console.WriteLine("5. МЕТОД Equals:");
+Console.WriteLine("================");
+
+MyNumber numA = new MyNumber(100);
+MyNumber numB = new MyNumber("C");
+MyNumber numC = new MyNumber(200);
+
+Console.WriteLine($"numA = {numA}, numB = {numB}, numC = {numC}");
+Console.WriteLine($"numA.Equals(numB): {numA.Equals(numB)}");
+Console.WriteLine($"numA.Equals(numC): {numA.Equals(numC)}");
+Console.WriteLine($"numA.Equals(\"строка\"): {numA.Equals("строка")}");
+
+Console.WriteLine();
+
+// 6. Демонстрация работы с отрицательными числами
+Console.WriteLine("6. ОТРИЦАТЕЛЬНЫЕ ЧИСЛА:");
+Console.WriteLine("======================");
+
+MyNumber neg1 = new MyNumber(-50);
+MyNumber neg2 = new MyNumber("-XXV"); // -25
+
+Console.WriteLine($"neg1 = {neg1}");
+Console.WriteLine($"neg2 = {neg2}");
+Console.WriteLine($"neg1 + neg2 = {neg1 + neg2}");
+Console.WriteLine($"neg1 - neg2 = {neg1 - neg2}");
